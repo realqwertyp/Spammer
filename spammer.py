816913b3ae417b85a73da0cb57492f8059b8c412
Geomty <69478549+Geomty@users.noreply.github.com>
2021-05-02 14:50:58 -0400

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -4,6 +4,7 @@ time.sleep(10) ##starts 5 seconds after running
 
 spamText = open("spam.txt", "r") ##spam.txt is the text you are using to spam
 
-for line in spamText:
-    pyautogui.typewrite(line)
-    pyautogui.press("enter")
+while (True):
+    for line in spamText:
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")
